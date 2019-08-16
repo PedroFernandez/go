59128fec6b08ba7096a6066011f415cfc2015922
PedroFernandez <fdez.pedro@gmail.com>
2019-08-16 20:26:39 +0200

    use NewFlagSet

--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
-var players = map[string]string{
-	"AMAKLPF52B4U44H0UZ4L": "Wu Lei",
-	"K43EXHJNGYDPN4LUD18N": "Víctor Sánchez",
-	"7FYRUXSZIDCD3NY38FDR": "Diego López",
+var beers = map[string]string{
+	"AMAKLPF52B4U44H0UZ4L": "Voll-Damm",
+	"K43EXHJNGYDPN4LUD18N": "Estrella Galicia",
+	"7FYRUXSZIDCD3NY38FDR": "Heineken",
 }
 
 func main() {
-	b := flag.Bool("players", false, "RCD Espanyol Players")
+	beersCmd := flag.NewFlagSet("beers", flag.ExitOnError)
 	flag.Parse()
-	if *b {
-		fmt.Println(players)
+
+	if flag.NArg() == 0 {
+		log.Fatal("Command beers must be included in the statement")
+		os.Exit(2)
+	}
+
+	switch flag.Arg(0) {
+	case "beers":
+		ID := beersCmd.String("id", "", "find by Id")
+
+		beersCmd.Parse(os.Args[2:])
+
+		if *ID != "" {
+			fmt.Println(beers[*ID])
+		} else {
+			fmt.Println(beers)
+		}
 	}
 }
